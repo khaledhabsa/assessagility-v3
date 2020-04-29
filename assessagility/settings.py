ee69987a453e000427bc3504eb1bf4847267d1c8
root <root@enghassan.enghassan.com>
2020-04-29 10:39:25 -0200

    update h home page

--- assessagility/settings.py
+++ assessagility/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c3oq*c(p6n)cv))(8u_d$)%!pueceq&bi*uc2pfulmz)j&pkuv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
