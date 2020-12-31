4c66eabdb634a83230ff31fc4df0059f7802ff28
tech.team <tech@tagerly.net>
2020-12-31 09:08:13 +0200

    allow waek password

--- survey/forms/user.py
+++ survey/forms/user.py
@@ -110,7 +110,7 @@ class AddUserForm(forms.ModelForm):
         fname = self.cleaned_data['first_name']
         lname = self.cleaned_data['last_name']
         pat_name = r'^[a-zA-z]+$'
-        pat_pass = r'^([a-zA-Z]+[0-9]+[!@#$%^&*(),.?":{}|<>_-]+|[a-zA-Z]+[!@#$%^&*(),.?":{}|<>_-]+[0-9]+|[!@#$%^&*(),.?":{}|<>_-]+[a-zA-Z]+[0-9]+|[!@#$%^&*(),.?":{}|<>_-]+[0-9]+[a-zA-Z]+|[0-9]+[a-zA-Z]+[!@#$%^&*(),.?":{}|<>_-]+|[0-9]+[!@#$%^&*(),.?":{}|<>_-]+[a-zA-Z]+)[a-zA-Z0-9!@#$%^&*(),.?":{}|<>_-]*$'
+#        pat_pass = r'^([a-zA-Z]+[0-9]+[!@#$%^&*(),.?":{}|<>_-]+|[a-zA-Z]+[!@#$%^&*(),.?":{}|<>_-]+[0-9]+|[!@#$%^&*(),.?":{}|<>_-]+[a-zA-Z]+[0-9]+|[!@#$%^&*(),.?":{}|<>_-]+[0-9]+[a-zA-Z]+|[0-9]+[a-zA-Z]+[!@#$%^&*(),.?":{}|<>_-]+|[0-9]+[!@#$%^&*(),.?":{}|<>_-]+[a-zA-Z]+)[a-zA-Z0-9!@#$%^&*(),.?":{}|<>_-]*$'
         if not re.match(pat_name, fname):
             raise forms.ValidationError(
                 "First Name should be contain characters only")
@@ -118,12 +118,12 @@ class AddUserForm(forms.ModelForm):
             raise forms.ValidationError(
                 "Last Name should be contain characters only")
 
-        if len(password) < 8:
-            raise forms.ValidationError(
-                "Password should be contain at least 8 combination of characters,numbers, !@#$%^&*(),.?\":{}|<>_- only")
-        if not re.match(pat_pass, password):
-            raise forms.ValidationError(
-                "Password should be contain combination of characters,numbers, !@#$%^&*(),.?\":{}|<>_- only")
+        # if len(password) < 8:
+        #     raise forms.ValidationError(
+        #         "Password should be contain at least 8 combination of characters,numbers, !@#$%^&*(),.?\":{}|<>_- only")
+        # if not re.match(pat_pass, password):
+        #     raise forms.ValidationError(
+        #         "Password should be contain combination of characters,numbers, !@#$%^&*(),.?\":{}|<>_- only")
         check_email = User.objects.filter(email=email)
         if check_email.exists():
             raise forms.ValidationError(u'%s already exists' % email)
